75226cfe1648374d9c664372410b7d007c20d396
Tetiana Stepanova <tetiana.stepanova@macys.com>
2022-08-28 19:42:14 +0300

    2 commit

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: './src/index.tsx',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'build'),
